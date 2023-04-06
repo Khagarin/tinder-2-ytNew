119d58a8aab09b69dbd80004f39cdea865305898
Mert Can Güven <c1812030@student.cankaya.edu.tr>
2023-04-06 16:32:33 +0300

    done screens

---
+++ screens/LoginScreen.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+
+const LoginScreen = () => {
+    const navigation = useNavigation();
+    return ( 
+        <View>
+            <View style={{height: 40}}/>
+            <Text style = {LoginStyles.HeaderText}>Login & Sign Up</Text>
+            <View style={{height: 80}}/>
+            
+        </View>
+    )
+}
+
+export default LoginScreen
+
+const LoginStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    infoText: {
+      fontSize: 20,
+      fontWeight: "500",
+      color: "black",
+      alignSelf: "center"
+    },
+    HeaderText: {
+      fontSize: 25,
+      fontWeight: "700",
+      color: "brown",
+      alignSelf: "center"
+      
+    }
+});
+
+/*
+<Button title = "Go to chat screen" />
+*/
