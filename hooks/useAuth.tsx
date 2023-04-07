39b57cb9d0899bdb99fac01543afd964c8e7275c
Mert Can Güven <c1812030@student.cankaya.edu.tr>
2023-04-07 17:44:53 +0300

    trying to firebase

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,14 +1,23 @@
 import React, { createContext, useContext } from "react";
 import { View, Text } from 'react-native';
+import * as Google from "expo-google-app-auth"
 
 const AuthContext = createContext({
     // initial state
 })
 
 export const AuthProvider = ({ children }) => {
+
+    const SignInWithGoogle = async() => {
+        await Google.logInAsync
+    }
+
+
+
+
     return (
         <AuthContext.Provider value = {{
-            user: "Mert Can"
+            user: null
         }}>
             {children}
         </AuthContext.Provider>
