cf7291281235f551c8376bd6525dfafc00a0084f
Mert Can Güven <c1812030@student.cankaya.edu.tr>
2023-04-10 13:51:35 +0300

    problems fixed, i guess

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,29 +1,50 @@
 import React, { createContext, useContext } from "react";
 import { View, Text } from 'react-native';
-import * as Google from "expo-google-app-auth"
+import { useAuthRequest } from 'expo-auth-session/providers/google';
+import { AuthSessionResult } from 'expo-auth-session';
+import { Button } from 'react-native';
 
 const AuthContext = createContext({
-    // initial state
+  // initial state
 })
 
-export const AuthProvider = ({ children }) => {
+type AuthProviderProps = {
+  children: React.ReactNode;
+}
 
-    const SignInWithGoogle = async() => {
-        await Google.logInAsync
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+
+  const [request, response, promptAsync] = useAuthRequest({
+    clientId: 'YOUR_CLIENT_ID_HERE',
+    scopes: ['profile', 'email'],
+    usePKCE: false,
+  });
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { authentication } = response;
+      console.log(authentication);
     }
+  }, [response]);
 
+  const SignInWithGoogle = async () => {
+    const result = await promptAsync();
+    if (result.type === 'success') {
+      // handle successful authentication
+      console.log('Authentication successful!');
+    }
+  }
 
-
-
-    return (
-        <AuthContext.Provider value = {{
-            user: null
-        }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{
+      user: null,
+      SignInWithGoogle: SignInWithGoogle,
+    }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default function useAuth() {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 }
